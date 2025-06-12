761ce91d62735c89dcc01bef72cbb248383f77fe
Paulo Craice <paulocraice@gmail.com>
2025-06-12 17:44:44 -0300

    QS FIXES

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
+import QuemSomos from '../pages/QuemSomos'
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/quem-somos" element={<QuemSomos />} />
       {/* More routes later like /anunciar, /produtos, etc */}
     </Routes>
   )
